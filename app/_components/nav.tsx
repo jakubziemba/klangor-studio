f3724a5632a663e5f2a9a94980377a9289f82df9
jakubziemba <ziemba.jak@gmail.com>
2023-11-21 21:11:11 +0100

    [FIX] Nav colors transition

--- app/_components/nav.tsx
+++ app/_components/nav.tsx
@@ -14,7 +14,6 @@ export default function Nav() {
   console.log("nav rendered");
   const ref = useRef<HTMLHeadElement>(null);
   const logoRef = useRef<SVGSVGElement>(null);
-  const [isBackground, setIsBackground] = useState(true);
   const { scrollY } = useScroll();
 
   useMotionValueEvent(scrollY, "change", (latest) => {
@@ -31,24 +30,19 @@ export default function Nav() {
       firstSectionHeight - (headerHeight !== undefined ? headerHeight / 2 : 0);
 
     if (latest > target && ref.current !== null) {
-      // setIsBackground(true);
-      ref.current.style.color = "black";
       ref.current.style.background = "white";
+      ref.current.style.color = "black";
       if (logoRef.current && latest > target) {
         svgPaths?.forEach((path) => {
           path.style.fill = "black";
-          // path.setAttribute("fill", "black");
-          path.style.transition = "fill 0.3s";
         });
       }
     }
     if (latest < target && ref.current !== null) {
-      // setIsBackground(false);
       ref.current && (ref.current.style.background = "transparent");
       ref.current && (ref.current.style.color = "white");
       svgPaths?.forEach((path) => {
         path.style.fill = "white";
-        // path.setAttribute("fill", "white");
       });
     }
   });
@@ -56,7 +50,7 @@ export default function Nav() {
   return (
     <header
       ref={ref}
-      className={`-mb-nav-height h-nav-height font-regular nav-mask sticky left-0 right-0 top-0 z-10 flex items-center bg-transparent text-white mix-blend-difference backdrop-blur-[2px] transition-[background] duration-[0.3s]`}
+      className={`-mb-nav-height h-nav-height font-regular nav-mask sticky left-0 right-0 top-0 z-10 flex items-center bg-transparent text-white mix-blend-difference backdrop-blur-[2px] transition-[background] duration-200`}
     >
       <div className="lg:grid-desktop flex items-center justify-between lg:px-6">
         <a href="/" className="lg:col-start-1">
