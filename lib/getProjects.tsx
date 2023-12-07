93111134516609afed419a8a77d2d2e851fe9807
jakubziemba <ziemba.jak@gmail.com>
2023-12-07 14:34:56 +0100

    (fix 1/x) comment out placeholder projects blocking build

--- lib/getProjects.tsx
+++ lib/getProjects.tsx
@@ -1,11 +1,9 @@
-import * as contentful from "contentful";
-
 const space = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
 const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
 
-const client = contentful.createClient({
-  space: space || "",
-  accessToken: accessToken || "",
+const client = require("contentful").createClient({
+  space: space,
+  accessToken: accessToken,
 });
 
 export async function getProjects() {
