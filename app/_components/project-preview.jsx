66a73723a1075e5c7a844c7f6f25626a423c521a
jakubziemba <ziemba.jak@gmail.com>
2023-12-20 16:44:01 +0100

    (feat) display project titles on img hover

--- app/_components/project-preview.jsx
+++ app/_components/project-preview.jsx
@@ -4,20 +4,25 @@ import Link from "next/link";
 import ContentfulImage from "./contentful-image";
 
 export default function ProjectPreview({ data }, children) {
-  console.log(data);
-
   return (
     <Link
       href={`/projekty/${data.fields.slug}`}
       scroll={false}
-      className="transition-opacity duration-300 ease-in-out hover:opacity-70"
+      className={`group relative transition-all duration-300 ease-in-out`}
     >
-      <ContentfulImage
-        src={data.fields.featuredImage.fields.file.url}
-        width={data.fields.featuredImage.fields.file.details.image.width}
-        height={data.fields.featuredImage.fields.file.details.image.width}
-        alt={data.fields.featuredImage.fields.title}
-      ></ContentfulImage>
+      <div className="color-white absolute inset-0 z-10 m-0 flex items-center justify-center bg-black bg-opacity-0 p-0 text-2xl opacity-100 transition-all duration-300 group-hover:visible group-hover:bg-opacity-50">
+        <p className="z-30 block text-2xl font-normal text-white opacity-0 transition-all duration-300 group-hover:opacity-100">
+          {data.fields.title}
+        </p>
+      </div>
+      <div>
+        <ContentfulImage
+          src={data.fields.featuredImage.fields.file.url}
+          width={data.fields.featuredImage.fields.file.details.image.width}
+          height={data.fields.featuredImage.fields.file.details.image.width}
+          alt={data.fields.featuredImage.fields.title}
+        ></ContentfulImage>
+      </div>
     </Link>
   );
 }
